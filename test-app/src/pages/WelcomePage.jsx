6f9af8692b5a8b342d2c6acce4702ae2f3517f4a
HrushikeshKaranth <45314142+HrushikeshKaranth@users.noreply.github.com>
2023-05-15 00:50:16 +0530

    .

--- test-app/src/pages/WelcomePage.jsx
+++ test-app/src/pages/WelcomePage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../assets/css/welcomePage.css'
 import { Icon } from '@iconify/react';
+import { Link } from 'react-router-dom';
 //-----
 function WelcomePage() {
   return (
@@ -15,14 +16,14 @@ function WelcomePage() {
         <div className='buttons'>
           <button className='btnType1'>
             <Icon icon="logos:google-icon" className='icon' />
-            <h4 className='bold'>Continue with Google</h4>
+            <h4 className='bold'><Link to={'/register'}>Continue with Google</Link></h4>
           </button>
           <button className='btnType1'>
             <Icon icon="mdi:at" className='primaryColor icon' />
-            <h4 className='bold'>Continue with Email</h4>
+            <h4 className='bold'><Link to={'/register'}>Continue with Email</Link></h4>
           </button>
         </div>
-        <h4 className='center'>Already have an account? <u className='primaryColor bold pointer'>Login</u></h4>
+        <h4 className='center'>Already have an account? <Link to={'/login'}><u className='bold pointer primaryColor'>Login</u></Link></h4>
       </section>
     </main>
   )
--- test-app/src/routes/allRoutes.js
+++ test-app/src/routes/allRoutes.js
@@ -1,5 +1,13 @@
+import ForgotPassword from "../pages/ForgotPassword";
+import LoginPage from "../pages/LoginPage";
+import RegisterPage from "../pages/RegisterPage";
+import RegisterPageOtp from "../pages/RegisterPageOtp";
 import WelcomePage from "../pages/WelcomePage";
 
 export const routes = [
     { id: 1, path: "/" , element: <WelcomePage/> },
+    { id: 2, path: "/register" , element: <RegisterPage/> },
+    { id: 3, path: "/register-otp" , element: <RegisterPageOtp/> },
+    { id: 4, path: "/login" , element: <LoginPage/> },
+    { id: 5, path: "/forgot-password" , element: <ForgotPassword/> },
 ]
