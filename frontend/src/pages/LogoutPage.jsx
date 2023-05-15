d6f7aff858795f8320570a4557f046bf72e0b2e7
HrushikeshKaranth <45314142+HrushikeshKaranth@users.noreply.github.com>
2023-05-15 20:37:44 +0530

    sockets

--- frontend/src/pages/LogoutPage.jsx
+++ frontend/src/pages/LogoutPage.jsx
@@ -1,20 +1,37 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Icon } from '@iconify/react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../helpers/context';
+import { io } from 'socket.io-client'
+const socket = io.connect('http://localhost:3001');// creating connection to server
 //-----
 function LogoutPage() {
     // navigation
     let navigate = useNavigate()
 
     // states
+    const [message, setMessage] = useState("")
+    const [messageReceived, setMessageReceived] = useState("")
     const [login, setLogin] = useContext(AuthContext)
 
+    // function to send messages
+    const sendMessage = () =>{
+        socket.emit('send_message', {message})
+    };
+
+    // listening to incoming messages
+    useEffect(()=>{
+        socket.on('receive_message', (data)=>{
+            setMessageReceived(data.message);
+        })
+    },[socket])
+    
     // function to handle logout
     function handleLogout() {
         localStorage.removeItem('auth'); // removing token variable from localstorage.
-        setLogin({ ...login, auth: false, token: '' }); 
+        setLogin({ ...login, auth: false, token: '' });
         console.log('Logged out!');
+        socket.send('hello');
         navigate('/'); // redirecting to welcome page.
     }
 
@@ -26,7 +43,17 @@ function LogoutPage() {
                 <h2 className='primaryColor bold'>Welcome</h2>
                 <h4>Save all your work and Logout!</h4>
             </header>
-
+            {/* sending message */}
+            <section>
+                <input type="text" placeholder='Message...'
+                onChange={(e)=>{setMessage(e.target.value)}} />
+                <button className='btnType2' onClick={sendMessage}>Send </button>
+            </section>
+            {/* displaying received message  */}
+            <section>
+                <h3>Message:</h3>
+                <h4>{messageReceived}</h4>
+            </section>
             {/* Logout */}
             <section>
                 <button className='btnType2 my5'
---
+++ server/index.js
@@ -0,0 +1,30 @@
+
+const express = require('express');
+const app = express();
+const http = require('http');
+const { Server } = require('socket.io')
+const cors = require('cors')
+
+app.use(cors());
+
+// creating server
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:3000',
+        methods: ["GET", "POST"]
+    }
+});
+
+// listening to incoming messages
+io.on("connection", (socket)=>{
+    console.log(`User Connected ${socket.id}`);
+    //broadcasting received messages to others
+    socket.on("send_message", (data)=>{
+        socket.broadcast.emit("receive_message", data)
+    })
+})
+
+server.listen(3001, ()=>{
+    console.log('Server Running!');
+})
